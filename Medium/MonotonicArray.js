16347b0c58f3af6108b21d34e9abdb31644832d9
Henrychang26 <henrychang26@gmail.com>
2022-11-29 22:45:37 -0800

    DataStructure

--- Medium/MonotonicArray.js
+++ Medium/MonotonicArray.js
@@ -9,57 +9,65 @@
 //   exclusively increasing; they simply don't decrease.
 //   Note that empty arrays and arrays of one element are monotonic.
 
-let array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
+// let array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
 // -1 - -5 >= 0; // -5 - - 10 >= 0
 // 1 - 5 <= 0; // 5 - 10 <= 0 // 10 - 1 <= 0 ? false : true
 //outout = true
 
-function isMonotonic(array) {
-  let i = 0;
-  let result = true;
+// function isMonotonic(array) {
+//   let i = 0;
+//   let result = true;
 
-  while (i < array.length - 1 && array[i] >= array[i + 1]) {
-    if (array[i] >= array[i + 1]) {
-      i++;
-    } else {
-      result = false;
-    }
-  }
-  while (i < array.length - 1 && array[i] <= array[i + 1]) {
-    if (array[i] <= array[i + 1]) {
-      i++;
-    } else {
-      result = false;
-    }
-  }
-  console.log(result);
-  return result;
-
-  while (i < array.length) {
-    if (array.length < 3) {
-      result = true;
-      break;
-    }
-    if (array[i] >= array[i + 1]) {
-      i++;
-    } else {
-      result = false;
-    }
-  }
-  return result;
-}
-
-isMonotonic(array);
-
-// function isMonotonicRecursive(array,idx){
-
+//   while (i < array.length - 1 && array[i] >= array[i + 1]) {
+//     if (array[i] >= array[i + 1]) {
+//       i++;
+//     } else {
+//       result = false;
+//     }
+//   }
+//   while (i < array.length - 1 && array[i] <= array[i + 1]) {
+//     if (array[i] <= array[i + 1]) {
+//       i++;
+//     } else {
+//       result = false;
+//     }
+//   }
+//   console.log(result);
+//   return result;
 // }
 
-for (let i = 0; i < array.length; i++) {
-  if (array[i] >= array[i + 1]) {
-    return result;
-  } else {
-    result = false;
+let array = [-1, 5, -10, -1100, -1100, -1101, -1102, -9001];
+isMonotonic(array);
+
+function isMonotonic(array) {
+  let increasing = true;
+  let decreasing = true;
+
+  for (let i = 1; i < array.length; i++) {
+    if (isIncreasing(array[i], array[i - 1])) decreasing = false;
+    console.log(
+      `array[i]: ${array[i]},array[i - 1]: ${
+        array[i - 1]
+      }, decreasing : ${decreasing} `
+    );
+    if (isDecreasing(array[i], array[i - 1])) increasing = false;
+    console.log(
+      `array[i]: ${array[i]},array[i - 1]: ${
+        array[i - 1]
+      }, increasing : ${increasing} `
+    );
   }
+  console.log(`final result : ${increasing || decreasing}`);
+  return increasing || decreasing;
 }
-return result;
+
+function isIncreasing(a, b) {
+  return a > b;
+}
+function isDecreasing(a, b) {
+  return a < b;
+}
+console.log(`true  || false :${true || false}`);
+console.log(`false || true  :${false || true}`);
+console.log(`true  || true  :${true || true}`);
+console.log(`false || false :${false || false}`);
